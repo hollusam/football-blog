aa3d28c097277ffb36a05ff3c5cd09ede4dc67eb
Hollusam <oluyomisamuels@gmail.com>
2023-12-30 12:59:51 +0000

    wrote jsx code for Home.js

---
+++ football-blog/src/pages/Home.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Article from '../components/Article';
+import LiveScore from '../components/LiveScore';
+import News from '../components/News';
+
+const Home = () => {
+  const [articles, setArticles] = useState([]);
+  const [news, setNews] = useState([]);
+  const [liveScores, setLiveScores] = useState([]);
+
+  useEffect(() => {
+    // Fetch data (replace with actual APIs)
+    const fetchData = async () => {
+      try {
+        const articlesResponse = await axios.get('/api/articles');
+        const newsResponse = await axios.get('/api/news');
+        const liveScoresResponse = await axios.get('/api/live-scores');
+
+        setArticles(articlesResponse.data);
+        setNews(newsResponse.data);
+        setLiveScores(liveScoresResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Football Blog</h1>
+      </div>
+
+      <div className="container">
+        <Link to="/team">Go to Team Page</Link>
+
+        <div>
+          <h2>Latest Articles</h2>
+          {articles.map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+        </div>
+
+        <div>
+          <h2>Latest News</h2>
+          {news.map((newsItem) => (
+            <News key={newsItem.id} newsItem={newsItem} />
+          ))}
+        </div>
+
+        <div>
+          <h2>Live Scores</h2>
+          {liveScores.map((score) => (
+            <LiveScore key={score.id} score={score} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
