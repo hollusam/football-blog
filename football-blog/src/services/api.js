e37f7185eaf07dd11ddf864383a537eeae586cf7
Hollusam <oluyomisamuels@gmail.com>
2023-12-30 13:27:12 +0000

    api.js updated

--- football-blog/src/services/api.js
+++ football-blog/src/services/api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
+const apiKey = '6510c0ff4a75454b80dde25e3936bd5c'; // Replace with your actual API key
+const baseURL = 'https://api.football-data.org/v2';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // Replace with your API server URL
+  baseURL,
+  headers: {
+    'X-Auth-Token': apiKey,
+  },
 });
 
 export const getArticles = () => api.get('/articles');
 export const getNews = () => api.get('/news');
-export const getLiveScores = () => api.get('/live-scores');
+export const getLiveScores = () => api.get('/matches/live');
+export const getFixtures = () => api.get('/matches');
+export const getTeamInfo = (teamId) => api.get(`/teams/${teamId}`);
