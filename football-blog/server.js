1eac2a7d3d3e9a17f985a2bcfc26ee7f9860bc3a
Hollusam <oluyomisamuels@gmail.com>
2023-12-30 13:54:08 +0000

    Data folder created and server updated

--- football-blog/server.js
+++ football-blog/server.js
@@ -4,23 +4,19 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use(express.json());
 
 // Mock data (replace with your own data)
 const articles = require('./data/articles.json');
-const news = require('./data/news.json');
-const liveScores = require('./data/live-scores.json');
 
+// API endpoints for mock data
 app.get('/api/articles', (req, res) => {
   res.json(articles);
 });
 
-app.get('/api/news', (req, res) => {
-  res.json(news);
-});
-
-app.get('/api/live-scores', (req, res) => {
-  res.json(liveScores);
+// Run your proxy server on http://localhost:3002
+// Update your frontend API calls to use this proxy server
+app.use('/api/football-data', (req, res) => {
+  req.pipe(request(`http://localhost:3002${req.url}`)).pipe(res);
 });
 
 app.listen(PORT, () => {
